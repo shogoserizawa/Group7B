8dc635bb9112cba781109bcbb802fa84ee3370a5
sunshine <sunshine1010yk@gmail.com>
2024-05-22 23:13:01 +0900

    リストに追加された各アイテムを削除できるように変更

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -41,7 +41,7 @@ function Home() {
   };
 
   return (
-    <div className="container">
+    <div className="container">{/*css用クラス名定義*/}
       <div className="top-section">
         <div className="chat-room">
           <ChatRoom
