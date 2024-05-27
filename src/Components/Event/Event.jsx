dab5bdc945e984531e470843262cd92a91829b74
shogoserizawa <104557880+shogoserizawa@users.noreply.github.com>
2024-05-27 21:44:40 +0900

    yuki's fix

--- src/Components/Event/Event.jsx
+++ src/Components/Event/Event.jsx
@@ -2,6 +2,17 @@ import React from "react";
 import "./Event.css";
 
 function Event(props) {
+
+  //yyyy-mm-dd形式からmm-dd形式に変換する関数
+  const formatDate = (dateString) => {
+    const [month, day] = dateString.split("-");
+    return `${parseInt(month, 10)}/${parseInt(day, 10)}`;
+  };
+
+  const handleCheckboxChange = (id) => {
+    props.delete(id);
+  };
+
   return (
     <div>
       <h2>Event List</h2>
@@ -11,10 +22,10 @@ function Event(props) {
           .filter((event) => event.isClick === 2)
           .map((event, index) => (
             // task.messageでオブジェクトの中のメッセージを表示。日時を表示したい場合はtask.date
-            <li key={index}>
+            <li key={event.id}>
               <label>
-                <input type="checkbox" onChange={() => props.delete(index)} />
-                {event.date.slice(-5)}
+                <input type="checkbox" onChange={() => handleCheckboxChange(event.id)} />
+                {formatDate(event.date.slice(-5))}
                 {event.message}
               </label>
             </li>
