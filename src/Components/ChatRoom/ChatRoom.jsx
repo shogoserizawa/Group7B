1ed06d26d58bd0bbaee2760bfbe744b1f59fdd46
Kokoro3636 <kokorohidari@gmail.com>
2024-05-29 20:23:40 +0900

    chatroomのメッセージ表示変更＆送信者アイコン追加

--- src/Components/ChatRoom/ChatRoom.jsx
+++ src/Components/ChatRoom/ChatRoom.jsx
@@ -125,18 +125,24 @@ const ChatRoom = (props) => {
           <button
             key={msg.id}
             onClick={() => handleClick(msg, index)}
-            className={msg.type === "0" ? "todo-message" : "event-message"}
+            className={`${
+              msg.sender === props.name ? "my-message" : "other-message"
+            } ${msg.type === "0" ? "todo-message" : "event-message"}`}
           >
-            {msg.sender !== "aaaaaaaaaa" /* 下のdivにcssつける */ && (
-              <div>
-                <span>{msg.message}</span> <span>{formatDate(msg.date)}</span>
+            <div className="parallel">
+              <div className="icon-msg-wrapper">
+                {msg.sender !== props.name && (
+                  <div className="sender-icon">
+                    <span>{msg.sender}</span>
+                  </div>
+                )}
+                <span id="message">{msg.message}</span>
               </div>
-            )}
-            {msg.sender === "aaaaaaaaaa" && (
-              <div>
-                <span>my messeage</span> <span>{formatDate(msg.date)}</span>
-              </div>
-            )}
+              <span>
+                {"| "}
+                {formatDate(msg.date)}
+              </span>
+            </div>
           </button>
         ))}
       </div>
